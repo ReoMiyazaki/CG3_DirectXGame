abc5bdb5286f7e31f74ca465d4ff2c2d357cc57f
REO MIYA <41591rionmiyazaki@gmail.com>
2022-11-27 23:22:16 +0900

    ビュー行列の実装

--- Object3d.h
+++ Object3d.h
@@ -101,6 +101,13 @@ public: // 静的メンバ関数
 	/// </summary>
 	/// <param name="move">移動量</param>
 	//static void CameraMoveVector(XMFLOAT3 move);
+	static void CameraMoveVector(XMFLOAT3 move);
+
+	/// <summary>
+	/// ベクトルによる移動
+	/// </summary>
+	/// <param name="move">移動量</param>
+	//static void CameraMoveVector(XMFLOAT3 move);
 	static void CameraMoveEyeVector(XMFLOAT3 move);
 
 private: // 静的メンバ変数
